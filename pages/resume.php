1ff67c84303d0fdad0f54dcd0b0054c24b03a17f
BakariWilkins <bxw2270@rit.edu>
2018-09-28 17:27:36 -0400

    made ind. project pages

--- pages/resume.php
+++ pages/resume.php
@@ -6,7 +6,7 @@
 ?>
     <main role="main" class="container" >
         <a href="<?=$rel_path_home?>assets/media/bakari_wilkins_resume.pdf" class="btn btn-primary btn-lg resume-download" download>Download Resume</a>
-        <div class='embed-responsive' style='padding-bottom:150%'>
+        <div class='embed-responsive' style='padding-bottom: 125%'>
             <embed src="<?=$rel_path_home?>assets/media/bakari_wilkins_resume.pdf" id="resume" type="application/pdf" width="auto" height="auto"/>
         </div>
 
